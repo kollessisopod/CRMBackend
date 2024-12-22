9a76e7449f0693a53700d1e5a29e06c429cc3a49
azizcifcibasi <168673299+azizcifcibasi@users.noreply.github.com>
2024-12-22 15:39:55 +0300

    sql

---
+++ sqlquery.sql
@@ -0,0 +1,73 @@
+DROP TABLE IF EXISTS player_game CASCADE;
+DROP TABLE IF EXISTS player CASCADE;
+DROP TABLE IF EXISTS game CASCADE;
+DROP TABLE IF EXISTS employee CASCADE;
+DROP TABLE IF EXISTS feedback CASCADE;
+DROP TABLE IF EXISTS campaign CASCADE;
+
+CREATE TABLE employee(
+	employee_id int not null primary key,
+	e_name varchar(20) not null,
+	e_password varchar(20) not null,
+	e_email varchar(20),
+	e_type boolean
+);
+
+CREATE TABLE player(
+	player_id int not null primary key,
+	p_name varchar(20) not null,
+	p_password varchar(20) not null,
+	p_email varchar(40) not null,
+	p_pnumber char(10) not null,
+	last_online date
+);
+
+CREATE TABLE game(
+	game_id int not null primary key,
+	game_name varchar(20) not null,
+	game_genre varchar(20) not null
+);
+
+CREATE TABLE player_game(
+	p_ID int not null,
+	g_ID int not null,
+	score int,
+	
+	CONSTRAINT fk_player FOREIGN KEY (p_ID)
+	REFERENCES player(player_id),
+	CONSTRAINT fk_game FOREIGN KEY (g_ID)
+	REFERENCES game(game_id),
+	CONSTRAINT score_ck CHECK (score<11 AND score>0)
+);
+
+CREATE TABLE feedback(
+	feedback_ID int not null primary key,
+	feedback_type varchar(20) not null,
+	feedback_info varchar(20) not null
+);
+
+CREATE TABLE campaign(
+	campaign_ID int not null primary key,
+	campaign_info varchar(50) not null,
+	hasReward boolean not null,
+	reward_info varchar(20)
+);
+
+INSERT INTO player VALUES (21011050, 'Ali Eren Arık', '12346789', 'eren.arik@std.yildiz.edu.tr', '5352528503', '2024-12-22');
+
+
+CREATE OR REPLACE FUNCTION get_all_players()
+RETURNS TABLE (
+    player_id INT,
+    p_name VARCHAR(20),
+    p_password VARCHAR(20),
+    p_email VARCHAR(20),
+    p_pnumber CHAR(10),
+    last_online DATE
+) AS $$
+BEGIN
+    RETURN QUERY
+    SELECT * FROM player;
+END;
+$$ LANGUAGE plpgsql;
+
