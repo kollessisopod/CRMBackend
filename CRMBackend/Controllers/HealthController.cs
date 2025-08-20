e5786763cccd8a1d3d25a081b903961b5f9e7155
K. Ata Turkoglu <turkoglu.kata@gmail.com>
2025-08-20 09:51:06 +0300

    health

---
+++ CRMBackend/Controllers/HealthController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CRMBackend.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class HealthController : ControllerBase
+{
+    //health check get request
+    [HttpGet]
+    public IActionResult Get()
+    {
+        return Ok(new { message = "Service is running" });
+    }
+
+}
